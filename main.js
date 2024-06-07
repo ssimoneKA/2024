1b0c90e327cffcb515393007c1b23229cf0692a7
ssimoneKA <130755354+ssimoneKA@users.noreply.github.com>
2024-06-07 08:31:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const botoes = document.querySelectorAll('.botao');
+document.querySelectorAll('.botao').forEach(btn => {
+  btn.addEventListener('click', function () {
+    document.querySelectorAll('.aba-conteudo').forEach(tab => {
+      tab.classList.remove('ativo');
+    });
+    const index = Array.from(document.querySelectorAll('.botao')).indexOf(this);
+    document.querySelectorAll('.aba-conteudo')[index].classList.add('ativo');
+  });
+});
 
-for(let i=0;i <botoes.length;i++){
- botoes[i].onclick = function() {
-for(let j=0;j<botoes.length;j++){
-botoes[j].classList.remove('ativo');
-}
-   botoes[i].classList.add('ativo');
- }
-}
+
+const botoes = document.querySelectorAll('.botao');
+for(let i=0;i ,botoes.length;i++){
+  botoes[i].onclick = function() {
+    for(let j=0;j<botoes.length;j++) {
+      botoes[j].classList.remove('ativo'); }
+    botoes[i].classList.add('ativo');} 
+    }   
